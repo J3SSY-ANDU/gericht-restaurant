e1a0a7348344b6e107c8615fce45464ac834af93
J3SSY-ANDU <jessy.andujar@upr.edu>
2024-03-30 17:34:16 -0400

    header image rendered on load update

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { images } from '../../constants';
 import './Header.css';
 import { SubHeading } from '../../components';
